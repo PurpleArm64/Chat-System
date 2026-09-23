81afebd4a39d0cb25a3ebb29af1600441c46787c
Purple System <matikero312@gmail.com>
2026-09-23 21:44:13 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chat-pwa-v.3.2;
+const CACHE_NAME = 'chat-pwa-v.32;
 
 self.addEventListener('install', () => { self.skipWaiting(); });
 self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
